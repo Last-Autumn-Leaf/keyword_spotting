54add6b5b53ecf08b375926301ec9098868509bb
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-06-22 18:06:44 -0400

    mise à jours des .sh

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from dataset.subsetSC import SubsetSC
 from utilsFunc import timeThat
+import sys
 
-
-def test_importing_dataset():
+def test_importing_dataset(root=None):
     print("testing importing dataset")
     with timeThat('Test sets'):
-        root='./'
+        if root is None :
+            root='./'
         test_set = SubsetSC("testing", root)
         print('test loader set up, size', len(test_set))
     print('import finished')
 
 if __name__=='__main__':
-    test_importing_dataset()
+    if len(sys.argv) >1 :
+        print('root set to',sys.argv[1])
+        r=sys.argv[1]
+    else :
+        r=None
+
+    test_importing_dataset(r)
