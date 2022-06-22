298500c6e0cf8796fd91b20b6394b43a1c686996
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-06-22 15:32:36 -0400

    mise à jours des notebooks

--- main_job.sh
+++ main_job.sh
@@ -2,5 +2,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --account=def-seanwood
 #SBATCH --output=./logs/slurm_%j.out
-#SBTACH --mem=16G
+#SBTACH --cpus-per-task=3
+#SBTACH --mem=12G
+source ~/venv/bin/activate
 python main.py --model mel --save_checkpoint 'mel' --num-epochs 10
--- simple_job.sh
+++ simple_job.sh
@@ -2,6 +2,9 @@
 #SBATCH --time=00:2:00
 #SBATCH --account=def-seanwood
 #SBATCH --output=./logs/slurm_%j.out
-#SBTACH --mem=16G
+#SBTACH --cpus-per-task=3
+#SBTACH --mem=12G
+source ~/venv/bin/activate
 python test.py
+deactivate
 sleep 30
