35891499f3045695df4f27293ae51b0e66a1bbb8
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-06-26 17:46:00 -0400

    Modified version

--- main_job.sh
+++ main_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 #SBATCH --account=def-seanwood
 #SBATCH --output=./logs/slurm_%j.out
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=16G
 source ~/venv/bin/activate
-python main.py --model mel --save_checkpoint 'mel' --num-epochs 10
+python main.py --model mel --save_checkpoint 'mel' --num-epochs 100
