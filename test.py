2e746a4e58853432daa596c22d7f8b9d7c8330f4
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-06-28 13:42:42 -0400

    fichier binaire pour les datasets

--- test.py
+++ test.py
@@ -44,13 +44,15 @@ if __name__=='__main__':
     with timeThat('reimporting dataset'):
         with open('./SpeechCommands/pickle/test_set.pt', 'rb') as handle:
             pickled_test_set = pickle.load(handle)
+            waveform, sample_rate, label, speaker_id, utterance_number = pickled_test_set[0]
+            print("test set imported")
         with open('./SpeechCommands/pickle/train_set.pt', 'rb') as handle:
             pickled_train_set = pickle.load(handle)
+            waveform, sample_rate, label, speaker_id, utterance_number = pickled_train_set[0]
+            print("train set imported")
         with open('./SpeechCommands/pickle/validation_set.pt', 'rb') as handle:
             pickled_validation_set = pickle.load(handle)
-
-    waveform, sample_rate, label, speaker_id, utterance_number = pickled_test_set[0]
-    waveform, sample_rate, label, speaker_id, utterance_number = pickled_train_set[0]
-    waveform, sample_rate, label, speaker_id, utterance_number = pickled_validation_set[0]
+            waveform, sample_rate, label, speaker_id, utterance_number = pickled_validation_set[0]
+            print("validation set imported")
 
     print("import successfull")
