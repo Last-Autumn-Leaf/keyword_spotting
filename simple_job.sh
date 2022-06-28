60601799f10e6963a93d01a128966b885b295416
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-06-28 13:37:04 -0400

    fichier binaire pour les datasets

--- simple_job.sh
+++ simple_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=10:00:00
 #SBATCH --account=def-seanwood
-#SBATCH --output=./logs/slurm_%j.out
+#SBATCH --output=./logs/test_%j.out
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=4G
--- test.py
+++ test.py
@@ -22,7 +22,7 @@ if __name__=='__main__':
     test_importing_dataset(r)'''
 
 
-    root = './'
+    '''root = './'
     with timeThat('init dataset') :
         train_set = SubsetSC("training",root=root)
         validation_set = SubsetSC("validation",root=root)
@@ -38,13 +38,19 @@ if __name__=='__main__':
         with open('./SpeechCommands/pickle/train_set.pt', 'wb') as handle:
             pickle.dump(train_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        with open('../SpeechCommands/pickle/validation_set.pt', 'wb') as handle:
-            pickle.dump(validation_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open('./SpeechCommands/pickle/validation_set.pt', 'wb') as handle:
+            pickle.dump(validation_set, handle, protocol=pickle.HIGHEST_PROTOCOL)'''
 
     with timeThat('reimporting dataset'):
         with open('./SpeechCommands/pickle/test_set.pt', 'rb') as handle:
             pickled_test_set = pickle.load(handle)
-        with open('../SpeechCommands/pickle/train_set.pt', 'rb') as handle:
+        with open('./SpeechCommands/pickle/train_set.pt', 'rb') as handle:
             pickled_train_set = pickle.load(handle)
-        with open('../SpeechCommands/pickle/validation_set.pt', 'rb') as handle:
+        with open('./SpeechCommands/pickle/validation_set.pt', 'rb') as handle:
             pickled_validation_set = pickle.load(handle)
+
+    waveform, sample_rate, label, speaker_id, utterance_number = pickled_test_set[0]
+    waveform, sample_rate, label, speaker_id, utterance_number = pickled_train_set[0]
+    waveform, sample_rate, label, speaker_id, utterance_number = pickled_validation_set[0]
+
+    print("import successfull")
