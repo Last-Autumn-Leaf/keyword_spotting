853c5c098e073503d4181a7092ba16f729a2cb64
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-07-26 20:13:15 -0400

    SBATCH modif

--- simple_job.sh
+++ simple_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=6:00:00
 #SBATCH --account=def-seanwood
+#SBATCH --array=1-10
 #SBATCH --output=./logs/test_%j.out
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=4G
-#SBATCH --array=1-10
 #Variables
 myDir="/home/yoogo/projects/def-seanwood/yoogo/mywork/keyword_spotting"
 archiveFile="$myDir/speech_commands_v0.02.tar.gz"
@@ -18,4 +18,4 @@ tar -xf $archiveFile --directory $archiveDir
 source ~/venv/bin/activate
 
 #---------------
-python main.py --model PDM --num-epochs 1 --exp_name delthis
+python test.py $SLURM_ARRAY_TASK_ID
--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 
 import sys
-import run_array.run_array_PDM as run_array
 
 if __name__=='__main__':
-    run_array.test()
-
-    '''if len(sys.argv) >1 :
-        print('job index ',sys.argv[1])'''
+    if len(sys.argv) >1 :
+        print('job index ',sys.argv[1])
