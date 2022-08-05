fdc2258d657eb2ad92011111b7bfc789ff8a9413
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-08-05 06:02:42 -0400

    new subsets

--- dataset/subsetPDM.py
+++ dataset/subsetPDM.py
@@ -12,10 +12,13 @@ from helper.utilsFunc import PdmTransform
 
 class SubsetPDM(Dataset):
     def __init__(self):
-        self.walker=[]
+        ...
 
-    def add(self,other):
-        self.walker.append(other)
+    def setData(self,data):
+        self.data=data
+
+    def setTarget(self,target):
+        self.target=target
 
     def label_to_index(self,word):
         # Return the position of the word in labels
@@ -27,7 +30,7 @@ class SubsetPDM(Dataset):
         return self.labels[index]
 
     def getLabels(self):
-        self.labels = sorted(list(set(datapoint[1] for datapoint in self.walker)))
+        self.labels = sorted(list(set(datapoint for datapoint in self.target)))
         return self.labels
 
     def collate_fn(self,batch):
@@ -43,8 +46,8 @@ class SubsetPDM(Dataset):
 
         return tensors, targets
 
-    def __len__(self): return len(self.walker)
-    def __getitem__(self, i): return self.walker[i]
+    def __len__(self): return len(self.target)
+    def __getitem__(self, i): return (self.data[i],self.target[i])
 
 white_list_mode=['training','testing','validation']
 def setupPDM(pdm_factor=20,mode='training'):
@@ -52,6 +55,7 @@ def setupPDM(pdm_factor=20,mode='training'):
         raise Exception('unrecognized mode: '+str(mode))
     fe = 16000
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device ="cpu"
     PDM_TRAMSFORM = PdmTransform(pdm_factor=pdm_factor, signal_len=fe,
                                  orig_freq=fe).to(device)
     root ='./'
@@ -60,19 +64,26 @@ def setupPDM(pdm_factor=20,mode='training'):
         print('Compute Canada detected, root set to', root)
 
     subset = SubsetSC(mode, root)
-    print('subset size :',len(subset))
+    print(mode,'subset size :',len(subset))
     n=len(subset)
     PDMsubset = SubsetPDM()
+    label,waveform=[],[]
     for i in range(n):
         temp=subset[i]
-        waveform=temp[0]
-        label =temp[2]
-        # padding :
-        waveform=subset.pad_sequence(waveform)
-        # Resample et PDM transform
-        waveform=PDM_TRAMSFORM (waveform)
+        label.append(temp[2])
+        waveform.append(temp[0])
 
-        PDMsubset.add(other=(waveform,label))
+    # padding :
+    print('padding...')
+    waveform = subset.pad_sequence(waveform)
+    # Resample et PDM transform
+    print('PDM-transforming...')
+    waveform = PDM_TRAMSFORM(waveform)
+
+    #saving
+    print('dumping')
+    PDMsubset.setData(waveform)
+    PDMsubset.setTarget(label)
     PDMsubset.getLabels()
     pickle.dump(PDMsubset, open("PDM_dataset_"+mode+'_'+str(pdm_factor)+".pt", "wb"))
 
@@ -85,9 +96,10 @@ if __name__=='__main__':
         mode=white_list_mode[index]
         setupPDM(pdm_factor=pdm_factor,mode=mode)
 
+        print('testing')
         PDMsubset = pickle.load(open("PDM_dataset_"+mode+'_'+str(pdm_factor)+".pt", "rb"))
         a=PDMsubset[0]
-        b=PDMsubset[0:100]
+        b=PDMsubset[0:9]
         print(a)
         print(b)
         print(len(PDMsubset))
--- test.py
+++ test.py
@@ -20,40 +20,7 @@ if device== "cuda":
 else:
     num_workers = 0
     pin_memory = False
-def test():
-    with timeThat('training/validation/tests sets' if not test_only else 'test set'):
-        if not test_only :
-            train_set = SubsetSC("training",'./')
-            train_loader = torch.utils.data.DataLoader(
-                train_set,
-                batch_size=b_size,
-                shuffle=True,
-                collate_fn=train_set.collate_fn,
-                num_workers=num_workers,
-                pin_memory=pin_memory, )
-            val_set = SubsetSC("validation", './')
-            val_loader = torch.utils.data.DataLoader(
-                val_set,
-                batch_size=b_size,
-                shuffle=True,
-                collate_fn=train_set.collate_fn,
-                num_workers=num_workers,
-                pin_memory=pin_memory, )
-        test_set = SubsetSC("testing", './')
-        test_loader = torch.utils.data.DataLoader(
-            test_set,
-            batch_size=b_size,
-            shuffle=True,
-            collate_fn=test_set.collate_fn,
-            num_workers=num_workers,
-            pin_memory=pin_memory, )
 
-    print('batch size :',b_size)
-    with timeThat('Every batches'):
-        for batch_idx, (data, target) in enumerate(test_loader):
-            with timeThat('\tbatch '+str(batch_idx)+'/'+str(len(test_loader))):
-                data = data.to(device)
-                data = PDM_TRAMSFORM(data)
 
 if __name__=='__main__':
     if len(sys.argv) > 1:
@@ -65,7 +32,7 @@ if __name__=='__main__':
 
         PDMsubset = pickle.load(open("PDM_dataset_" + mode + '_' + str(pdm_factor) + ".pt", "rb"))
         a = PDMsubset[0]
-        b = PDMsubset[0:100]
+        b = PDMsubset[0:9]
         print(a)
         print(b)
         print(len(PDMsubset))
